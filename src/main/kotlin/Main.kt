3b7b39f6dd5a5f4224c6fc8e7c3d38e5c089a9e4
ioanapravai <ioana.pravai99@gmail.com>
2022-08-07 18:35:26 +0300

    Main: Fixup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,13 @@ import model.XforYRule
 import service.CheckoutService
 
 fun main(args: Array<String>) {
-    val pricingRule1 = XforYRule(Consts.VOUCHER_CODE, 2, 1)
-    val pricingRule2 = UnitThresholdPriceRule(Consts.TSHIRT_CODE, 3, 19.0)
-    val standardPrice = StandardPriceRule(Consts.PANTS_CODE)
+
+    // Each type of product will have a pricing rule.
+    // If a special rule does not exist as business requirement,
+    // we add a standard pricing rule for that product.
+    val voucherRule = XforYRule(Consts.VOUCHER_CODE, 2, 1)
+    val tshirtRule = UnitThresholdPriceRule(Consts.TSHIRT_CODE, 3, 19.0)
+    val pantsStandardRule = StandardPriceRule(Consts.PANTS_CODE)
 
     val giftCard = Product(Consts.VOUCHER_CODE, "Gift Card", 5.0)
     val summerTShirt = Product(Consts.TSHIRT_CODE, "Summer T-Shirt", 20.0)
@@ -16,7 +20,7 @@ fun main(args: Array<String>) {
 
     var total = 0.0
 
-    val checkoutService = CheckoutService(listOf(pricingRule1, pricingRule2, standardPrice))
+    val checkoutService = CheckoutService(listOf(voucherRule, tshirtRule, pantsStandardRule))
 
     total = checkoutService.scan(giftCard)
     println("total: ")
